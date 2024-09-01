ad0bc80cd777d3f626553115ef539916e769cd12
allisonRabia <09snippet-frazzle@icloud.com>
2024-09-01 16:56:26 -0500

    tests pass, GA6 complete minus css

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,9 @@ import satData from "./components/satData";
 
 function App() {
   const [sat, setSat] = useState(satData);
-  //sat used to compare changes in state
-  //setSat is a function that will be used to update the state
+
   const displaySats = [...new Set(satData.map((data) => data.orbitType))];
-  //This variable will use the Set method to create a new array that holds
-  //unique elements. This function will be used to create the buttons, so we
-  // want to pull out the orbitType values of each object inside satData. The
-  //Set method prevents duplicate elements. We are using it because there are
-  //only three types of orbitTypes: Low, Medium, and High. This will eventually create three buttons. If we only used map, we would create 10 buttons, and many of them would be duplicates.
+
   const filterByType = (currentType) => {
     const displaySats = satData.filter((newSatDisplay) => {
       return newSatDisplay.orbitType === currentType;
@@ -23,9 +18,9 @@ function App() {
   return (
     <div>
       <Banner
-        filterByType={filterByType}
-        setSat={setSat}
-        displaySats={displaySats}
+      // filterByType={filterByType}
+      // setSat={setSat}
+      // displaySats={displaySats}
       />
       <Buttons
         filterByType={filterByType}
