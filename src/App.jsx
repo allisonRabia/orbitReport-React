587373a7945a81eb1dba59994ccb8b530ef3dec3
allisonRabia <09snippet-frazzle@icloud.com>
2024-09-01 16:05:07 -0500

    all orbits not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
+import { useState } from "react";
+import satData from "./components/satData";
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  //sat used to compare changes in state
+  //setSat is a function that will be used to update the state
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  //This variable will use the Set method to create a new array that holds
+  //unique elements. This function will be used to create the buttons, so we
+  // want to pull out the orbitType values of each object inside satData. The
+  //Set method prevents duplicate elements. We are using it because there are
+  //only three types of orbitTypes: Low, Medium, and High. This will eventually create three buttons. If we only used map, we would create 10 buttons, and many of them would be duplicates.
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+  };
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />
     </div>
   );
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Banner extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>Orbit Report</header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+      </div>
+    );
+  }
+}
+
+export default Banner;
